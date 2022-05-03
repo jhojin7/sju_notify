bc972d081ce5ceb4fdb9e2ff25351e0e2d9c880f
Hojin Jang <jhojin7@gmail.com>
2022-05-03 13:53:08 +0900

    Send out notifications

--- etc.py
+++ etc.py
@@ -1,15 +1,25 @@
 import json
 
 def json_read(file:str)->dict:
+    """ Read json data to file. 
+    Returns dict if successful, False if not. """
     with open(file,'r',encoding="utf-8") as f:
         data = json.loads(f.read())
         f.close()
         return data
+    return False
 
-def json_write(file:str,data:dict):
-    f = open(file,'w')
-    f.write(json.dumps(data,ensure_ascii=False,indent=4))
-    f.close()
+def json_write(file:str,data:dict)->bool:
+    """ Write dict type data to file. 
+    Returns True if write successful, False if not. """
+    try:
+        with open(file,'w',encoding="utf-8") as f:
+            f.write(json.dumps(data,ensure_ascii=False,indent=4))
+            f.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 def log_append(DIR, LOG, all_new_notices):
     log_f = open(DIR+'/log.txt','a')
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from datetime import datetime
 import os
-import boards#, notify, etc
+import boards, notify, etc
 
 testdata_path = os.path.join(os.path.abspath(""),'tests','data.json')
-# DIR = '/home/pi/CODE/sju_notify'
-DIR = "."
 LOG = f"log: {datetime.today()}\n"
 # boardNames = ['main','haksa','chuiup','janghak']
-datajson = ''
-# data = etc.json_read(DIR+'/data.json')
 boardIds = {
     333:"main",
     335:"haksa",
@@ -19,21 +15,20 @@ boardIds = {
 # MAIN
 if __name__ == '__main__':
     ### check for update
-    all_new_notices = []
     for boardId in boardIds.keys():
         print("==", boardId, boardIds[boardId], "==")
         # TODO: DB도 해결해야함. sqlite3로할지 아니면 mongodb할지?
         new = boards.fetch_board(boardId)
         for _ in new: print(_)
-        # data[name] += new_notices
-        # ### sort by pkid (oldest on top)
-        # data[name].sort(key=(lambda x: x['pkid']))
-
-
-        # ### alert new_notices
-        # for notice in new_notices:
-        #     notify.alert(etc.make_message(notice))
-        # all_new_notices += new_notices
+        # send out notifications 
+        for notice in new:
+            notify.alert(notify.make_message(notice))
+        # append to db
+        db = etc.json_read(testdata_path)
+        db[boardIds[boardId]] += new
+        is_success = etc.json_write(testdata_path, db)
+        if not is_success:
+            print(">> !!!!!!!!! WRITE FAILED !!!!!!!!!")
 
     # etc.log_append(DIR, LOG, all_new_notices)
     
